58076b1740dde7feddd969e4964200a81c220612
c21rebja <c21rebja@student.his.se>
2022-04-08 14:32:20 +0200

    tested if the class worked

--- app/src/main/java/com/example/datatypesandlayout/MainActivity.java
+++ app/src/main/java/com/example/datatypesandlayout/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.datatypesandlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Mountain testMount = new Mountain(670, "Pleasant Mountain", "Antarctic", true, 3402);
+        Log.d("enTagg", testMount.getName());
     }
 }
